47e1fd2aae0961351c534d4439d4b887e96fd051
shaobo <shaobo123_ok@163.com>
2015-11-17 14:17:28 +0800

    更新

--- zhaopin/frontend/controllers/IndustryController.php
+++ zhaopin/frontend/controllers/IndustryController.php
@@ -9,22 +9,30 @@ class IndustryController extends \yii\web\Controller
      */
     public function actionGetlist(){
         $request = \Yii::$app->request;
-        /*if( $request->get('appid') != md5('zhaopin') ){
-            $data['status'] = 3;
-            $data['desc']   = 'appid错误';
+        if( $request->get('appid') != md5('zhaopin') ){
+            $data['status'] = '103';
+            $data['desc']   = '令牌错误';
             return json_encode($data);
-        }*/
+        }
         $list = Industry::find()->where('pid=0')->asArray()->all();
-        $industryList = array();
-        $i = 0;
-        foreach ( $list as $val ){
-            $industryList[$i][0] = $val;
-            $industryList[$i]['child_industry'] = Industry::find()->where('pid='.$val['id'])->asArray()->all();
-           
-            $i++;
+        if($list){
+            $industryList = array();
+            $i = 0;
+            foreach ( $list as $val ){
+                $industryList[$i][0] = $val;
+                $industryList[$i]['child_industry'] = Industry::find()->where('pid='.$val['id'])->asArray()->all();
+
+                $i++;
+            }
+            $data['status'] = '200';
+            $data['desc']   = '数据接收成功';
+            $data['data']   = $industryList;
+        }else{
+            $data['status'] = '101';
+            $data['desc']   = '数据未找到';
         }
         
-        return json_encode($industryList);
+        return json_encode($data);
        
         
     }
--- zhaopin/frontend/controllers/PositionController.php
+++ zhaopin/frontend/controllers/PositionController.php
@@ -15,29 +15,37 @@ class PositionController extends \yii\web\Controller
      */
     public function actionGetlist(){
         $request = \Yii::$app->request;
-        /*if( $request->get('appid') != md5('zhaopin') ){
-            $data['status'] = 3;
+        if( $request->get('appid') != md5('zhaopin') ){
+            $data['status'] = '103';
             $data['desc']   = 'appid错误';
             return json_encode($data);
-        }*/
+        }
         $list = Positions::find()->where('pid=0')->asArray()->all();
         $positionList = array();
         $i = 0;
-        
-        foreach ( $list as $val ){
-            $positionList[$i][0] = $val;
-            $positionList[$i]['child_position'] = Positions::find()->where('pid='.$val['id'])->asArray()->all();
-            
-            $j = 0;
-            foreach( $positionList[$i]['child_position'] as $val ){
+        if($list){
+            foreach ( $list as $val ){
+                $positionList[$i][0] = $val;
+                $positionList[$i]['child_position'] = Positions::find()->where('pid='.$val['id'])->asArray()->all();
 
-                $positionList[$i]['child_position'][$j]['child_position'] = Positions::find()->where('pid='.$val['id'])->asArray()->all();
-                $j++;
+                $j = 0;
+                foreach( $positionList[$i]['child_position'] as $val ){
+
+                    $positionList[$i]['child_position'][$j]['child_position'] = Positions::find()->where('pid='.$val['id'])->asArray()->all();
+                    $j++;
+                }
+                $i++;
             }
-            $i++;
+            
+            $data['status'] = '200';
+            $data['desc']   = '数据接收成功';
+            $data['data']   = $positionList;
+        }else{
+            $data['status'] = '101';
+            $data['desc']   = '数据未找到';
         }
-        print_r($positionList);die;
-        return json_encode($positionList);
+
+        return json_encode($data);
     }
 
 }
